c2833bd41b2064c89a881a45c4e5e0b6764452c9
Ju Liu <ju.liu@welaika.com>
2015-07-29 15:28:27 +0100

    Add font awesome icons

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+
 module.exports = {
-  name: 'ember-magellan'
+  name: 'ember-magellan',
+
+  included: function(app) {
+    this._super.included(app);
+
+    app.import(path.join(app.bowerDirectory, 'font-awesome/fonts/fontawesome-webfont.ttf'), {
+      destDir: 'assets/fonts'
+    });
+
+    app.import(path.join(app.bowerDirectory, 'font-awesome/fonts/fontawesome-webfont.woff'), {
+      destDir: 'assets/fonts'
+    });
+
+    app.import(path.join(app.bowerDirectory, 'font-awesome/fonts/fontawesome-webfont.woff2'), {
+      destDir: 'assets/fonts'
+    });
+
+    app.import(path.join(app.bowerDirectory, 'font-awesome/fonts/fontawesome-webfont.svg'), {
+      destDir: 'assets/fonts'
+    });
+
+    app.import(path.join(app.bowerDirectory, 'font-awesome/fonts/fontawesome-webfont.eot'), {
+      destDir: 'assets/fonts'
+    });
+  }
 };
